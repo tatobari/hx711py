2480b83b680ddb86cda1c0724547f26d1b3d419e
tatobari <tatobari@users.noreply.github.com>
2016-09-11 16:21:19 -0300

    Improved comments.

--- example.py
+++ example.py
@@ -15,11 +15,16 @@ hx = HX711(5, 6)
 # Still need to figure out why does it change.
 # If you're experiencing super random values, change these values to MSB or LSB until to get more stable values.
 # There is some code below to debug and log the order of the bits and the bytes.
+# The first parameter is the order in which the bytes are used to build the "long" value.
+# The second paramter is the order of the bits inside each byte.
+# According to the HX711 Datasheet, the second parameter is MSB so you shouldn't need to modify it.
 hx.set_reading_format("LSB", "MSB")
 
-# To set the reference unit comment the "hx.set_reference_unit(92)" line, put 1kg on your sensor
-# or some weight that you know and use rule of thirds to calculate the reference unit.
-# In this case, 92 is 1 gram.
+# HOW TO CALCULATE THE REFFERENCE UNIT
+# To set the reference unit to 1. Put 1kg on your sensor or anything you have and know exactly how much it weights.
+# In this case, 92 is 1 gram because, with 1 as a reference unit I got numbers near 0 without any weight
+# and I got numbers around 184000 when I added 2kg. So, according to the rule of thirds:
+# If 2000 grams is 184000 then 1000 grams is 184000 / 2000 = 92.
 hx.set_reference_unit(92)
 
 hx.reset()
@@ -27,7 +32,9 @@ hx.tare()
 
 while True:
     try:
-        # This is usefull to debug wether to use MSB or LSB in the reading formats.
+        # These three lines are usefull to debug wether to use MSB or LSB in the reading formats
+        # for the first parameter of "hx.set_reading_format("LSB", "MSB")".
+        # Comment the two lines "val = hx.get_weight(5)" and "print val" and uncomment the three lines to see what it prints.
         #np_arr8_string = hx.get_np_arr8_string()
         #binary_string = hx.get_binary_string()
         #print binary_string + " " + np_arr8_string
