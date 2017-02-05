a4413accc3f7ece1b61e27a2ee906da8fe2b5be8
tatobari <tato@convoy.me>
2017-02-05 20:49:56 +0000

    Added reference for small 20kg cells.

--- example.py
+++ example.py
@@ -25,6 +25,7 @@ hx.set_reading_format("LSB", "MSB")
 # In this case, 92 is 1 gram because, with 1 as a reference unit I got numbers near 0 without any weight
 # and I got numbers around 184000 when I added 2kg. So, according to the rule of thirds:
 # If 2000 grams is 184000 then 1000 grams is 184000 / 2000 = 92.
+#hx.set_reference_unit(113)
 hx.set_reference_unit(92)
 
 hx.reset()
