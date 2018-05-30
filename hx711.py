f2c0b1c9921b2e1deaca67dea9ffe51801f35298
Adam Dorsey <adam.dorsey@gmail.com>
2018-05-30 15:44:33 +0000

    Formatting cleanup

--- hx711.py
+++ hx711.py
@@ -2,7 +2,9 @@ import RPi.GPIO as GPIO
 import time
 import numpy  # sudo apt-get python-numpy
 
+
 class HX711:
+
     def __init__(self, dout, pd_sck, gain=128):
         self.PD_SCK = pd_sck
         self.DOUT = dout
@@ -37,8 +39,10 @@ class HX711:
 
         time.sleep(1)
 
+
     def is_ready(self):
         return GPIO.input(self.DOUT) == 0
+
 
     def set_gain(self, gain):
         if gain is 128:
@@ -68,12 +72,14 @@ class HX711:
             ret.append(False)
         return ret
 
+
     def read(self):
         while not self.is_ready():
-            #print("WAITING")
+            # print("WAITING")
             pass
 
-        dataBits = [self.createBoolList(), self.createBoolList(), self.createBoolList()]
+        dataBits = [
+            self.createBoolList(), self.createBoolList(), self.createBoolList()]
         dataBytes = [0x0] * 4
 
         for j in range(self.byte_range_values[0], self.byte_range_values[1], self.byte_range_values[2]):
@@ -83,18 +89,19 @@ class HX711:
                 GPIO.output(self.PD_SCK, False)
             dataBytes[j] = numpy.packbits(numpy.uint8(dataBits[j]))
 
-        #set channel and gain factor for next reading
+        # set channel and gain factor for next reading
         for i in range(self.GAIN):
             GPIO.output(self.PD_SCK, True)
             GPIO.output(self.PD_SCK, False)
 
-        #check for all 1
-        #if all(item is True for item in dataBits[0]):
+        # check for all 1
+        # if all(item is True for item in dataBits[0]):
         #    return long(self.lastVal)
 
         dataBytes[2] ^= 0x80
 
         return dataBytes
+
 
     def get_binary_string(self):
         binary_format = "{0:b}"
@@ -106,23 +113,26 @@ class HX711:
             binary_string += binary_segment + " "
         return binary_string
 
+
     def get_np_arr8_string(self):
         np_arr8 = self.read_np_arr8()
-        np_arr8_string = "[";
+        np_arr8_string = "["
         comma = ", "
         for i in range(4):
             if i is 3:
                 comma = ""
             np_arr8_string += str(np_arr8[i]) + comma
-        np_arr8_string += "]";
+        np_arr8_string += "]"
 
         return np_arr8_string
+
 
     def read_np_arr8(self):
         dataBytes = self.read()
         np_arr8 = numpy.uint8(dataBytes)
 
         return np_arr8
+
 
     def read_long(self):
         np_arr8 = self.read_np_arr8()
@@ -131,12 +141,14 @@ class HX711:
 
         return long(self.lastVal)
 
+
     def read_average(self, times=3):
         values = long(0)
         for i in range(times):
             values += self.read_long()
 
         return values / times
+
 
     # A median-based read method, might help when getting random value spikes
     # for unknown or CPU-related reasons
@@ -147,12 +159,15 @@ class HX711:
 
         return numpy.median(values)
 
+
     # Compatibility function, uses channel A version
     def get_value(self, times=3):
         return self.get_value_A(times)
 
+
     def get_value_A(self, times=3):
         return self.read_median(times) - self.OFFSET_A
+
 
     def get_value_B(self, times=3):
         # for channel B, we need to set_gain(32)
@@ -162,23 +177,28 @@ class HX711:
         self.set_gain(g)
         return value
 
+
     # Compatibility function, uses channel A version
     def get_weight(self, times=3):
         return self.get_weight_A(times)
+
 
     def get_weight_A(self, times=3):
         value = self.get_value_A(times)
         value = value / self.REFERENCE_UNIT_A
         return value
 
+
     def get_weight_B(self, times=3):
         value = self.get_value_B(times)
         value = value / self.REFERENCE_UNIT_B
         return value
 
+
     # Sets tare for channel A for compatibility purposes
     def tare(self, times=15):
         self.tare_A(times)
+
 
     def tare_A(self, times=15):
         # Backup REFERENCE_UNIT value
@@ -189,6 +209,7 @@ class HX711:
         self.set_offset_A(value)
 
         self.set_reference_unit_A(reference_unit)
+
 
     def tare_B(self, times=15):
         # Backup REFERENCE_UNIT value
@@ -205,6 +226,7 @@ class HX711:
         self.set_gain(g)
         self.set_reference_unit_B(reference_unit)
 
+
     def set_reading_format(self, byte_format="LSB", bit_format="MSB"):
         if byte_format == "LSB":
             self.byte_range_values = self.LSByte
@@ -216,39 +238,49 @@ class HX711:
         elif bit_format == "MSB":
             self.bit_range_values = self.MSBit
 
+
     # sets offset for channel A for compatibility reasons
     def set_offset(self, offset):
         self.set_offset_A(offset)
+
 
     def set_offset_A(self, offset):
         self.OFFSET_A = offset
         self.OFFSET = offset
 
+
     def set_offset_B(self, offset):
         self.OFFSET_B = offset
+
 
     # sets reference unit for channel A for compatibility reasons
     def set_reference_unit(self, reference_unit):
         self.set_reference_unit_A(reference_unit)
 
+
     def set_reference_unit_A(self, reference_unit):
         self.REFERENCE_UNIT_A = reference_unit
         self.REFERENCE_UNIT = reference_unit
 
+
     def set_reference_unit_B(self, reference_unit):
         self.REFERENCE_UNIT_B = reference_unit
 
+
     # HX711 datasheet states that setting the PDA_CLOCK pin on high for >60 microseconds would power off the chip.
     # I used 100 microseconds, just in case.
-    # I've found it is good practice to reset the hx711 if it wasn't used for more than a few seconds.
+    # I've found it is good practice to reset the hx711 if it wasn't used for
+    # more than a few seconds.
     def power_down(self):
         GPIO.output(self.PD_SCK, False)
         GPIO.output(self.PD_SCK, True)
         time.sleep(0.0001)
 
+
     def power_up(self):
         GPIO.output(self.PD_SCK, False)
         time.sleep(0.4)
+
 
     def reset(self):
         self.power_down()
