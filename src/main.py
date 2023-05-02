cec155a2069e775e5588833b00a228136b72e5e2
wbuz24 <wbuziak@vols.utk.edu>
2023-05-02 15:30:46 -0400

    saving images to a folder and pngdelete function

--- src/main.py
+++ src/main.py
@@ -1,10 +1,11 @@
 import pigpio # http://abyz.co.uk/rpi/pigpio/python.html
 import time # keep track of time
 import sys # close window and exit program
+import os # delete contents of a folder
 import numpy as np # vectors
 import pandas as pd # dataframes
 import matplotlib.pyplot as plt # plotting
-import matplotlib.animation as ani # animated plotting
+import imageio # animation
 from PyQt5.QtWidgets import * # GUI window
 
 class HX711: # amplifier class
@@ -20,7 +21,7 @@ class HX711: # amplifier class
     # this long it indicates that a new reading is available.
     TIMEOUT = ((X_64_CLK + 3) * 2 * PULSE_LEN)
     # The number of readings to skip after a mode change.
-    SETTLE_READINGS = 5 # normally just one mode
+    SETTLE_READINGS = 4 # normally just one mode
     def __init__(self, pi, DATA_PIN, CLOCK_PIN, mode=CH_A_GAIN_128, callback=None):
         """
         Instantiate with the Pi, the data GPIO, and the clock GPIO.
@@ -192,6 +193,12 @@ def cleanAndExit(): # exit protocol
 
     sys.exit()
 
+def pngdelete(): # delete all instances of pngs within the "img" folder
+    filelist = [f for f in os.listdir("img") if f.endswith(".png") ]
+
+    for f in filelist:
+        os.remove(os.path.join("img", f))
+
 def window(): # define window instance and buttons
     app = QApplication([]) # create a window
     window = QWidget()
@@ -211,21 +218,29 @@ def window(): # define window instance and buttons
     window.show() # show window
     app.exec_()
 
-def animate(frame): # animate a graph
-    lines.set_data(x, y)
-
-def graph(res, xm, ym): # graphing function
+def graph(t, x, y): # graphing function
     # xm is the maximum x value to graph and ym is the max y value
-    # res is the list passed to the function containing the center of mass for each timestep
-    Figure = plt.figure()
+    # t is the current index
+
+    fig = plt.figure(figsize=(6, 6)) # create a figure instance
+    plt.plot(x[:(t+1)], y[:(t+1)], color = 'grey')
+    plt.plot(x[t], y[t], color = 'black', marker = 'o')
+
+    plt.xlim(-max(x) - max(x) * .1, max(x) + max(x) * .1) # xlimits
+    plt.ylim(-max(y) - max(y) * .1, max(y) + max(y) * .1) # ylimits
+
+    plt.title('Center of gravity', fontsize = 14)
+
+    plt.savefig(f'./img/img_{t}.png', transparent = False, facecolor = 'white')
+    plt.close()
 
     # create a plot
-    lines = plt.plot([])
+#    lines = plt.plot([])
 
     # limits
-    lines = lines[0]
-    plt.xlim(-xm, xm)
-    plt.ylim(-ym, ym)
+#    lines = lines[0]
+#    plt.xlim(-xm, xm)
+#    plt.ylim(-ym, ym)
 
 
 if __name__ == "__main__": # main function
@@ -235,14 +250,21 @@ if __name__ == "__main__": # main function
         def __init__(self, pi):
             self.last_count1 = -1
             self.last_count2 = -1
+            self.last_count3 = -1
+            self.last_count4 = -1
             self.scale_read1 = 0
             self.scale_read2 = 0
+            self.scale_read3 = 0
+            self.scale_read4 = 0
             self.s1 = HX711(pi, DATA_PIN=20, CLOCK_PIN=21, mode=HX711.CH_A_GAIN_128, callback=self.scale1_read_cb)
             self.s2 = HX711(pi, DATA_PIN=13, CLOCK_PIN=19, mode=HX711.CH_A_GAIN_128, callback=self.scale2_read_cb)
+            self.s3 = HX711(pi, DATA_PIN=14, CLOCK_PIN=15, mode=HX711.CH_A_GAIN_128, callback=self.scale3_read_cb)
+            self.s4 = HX711(pi, DATA_PIN=2, CLOCK_PIN=3, mode=HX711.CH_A_GAIN_128, callback=self.scale4_read_cb)
 
             self.s1.start()
-            time.sleep(2)
             self.s2.start()
+            self.s3.start()
+            self.s4.start()
             time.sleep(2)
             print("both start!!")
 
@@ -255,8 +277,23 @@ if __name__ == "__main__": # main function
             if count!=self.last_count2:
                 self.scale_read2 = reading
                 self.last_count2 = count
+
+        def scale3_read_cb(self, count, mode, reading):
+            if count!=self.last_count3:
+                self.scale_read3 = reading
+                self.last_count3 = count
+
+        def scale4_read_cb(self, count, mode, reading):
+            if count!=self.last_count4:
+                self.scale_read4 = reading
+                self.last_count4 = count
+
     def getVals():
-        s1 = [] # lists for saving
+        x = [] # lists for saving
+        y = []
+        x_length = 9
+        y_length = 5
+        pngdelete() # delete all png files
 
         pi = pigpio.pi() # pi instance
         if not pi.connected:
@@ -270,17 +307,27 @@ if __name__ == "__main__": # main function
             start = time.time()
             op = 1
             while op: # main loop
-                if time.time() - start > 10:
+                if time.time() - start > 12:
                     op = 0
                 else:
-                    scale1 = weighing_system.scale_read1 # read each number
-                    scale2 = weighing_system.scale_read2
-                    print("scale1: ", scale1, "scale2: ", scale2)
-                    #print("scale1:", weighing_system.scale_read1)
-                    #print("scale2:", weighing_system.scale_read2)
-                    # start = time.time()
+                    scale1 = weighing_system.scale_read1 # read top left
+                    #print("scale1:", scale1)
+                    scale2 = weighing_system.scale_read2 # bottom left
+                    scale3 = weighing_system.scale_read3 # top right
+                    scale4 = weighing_system.scale_read4 # bottom right
+
+                    #rtotal = scale1 + scale2 + scale3 + scale4 # total weight
                     
-                    s1.append()
+                    #rl = (((scale3 + scale4) * x_length) - ((scale1 + scale2) * x_length)) / rtotal
+                    #th = (((scale3 + scale1) * y_length) - ((scale2 + scale3) * y_length)) / rtotal
+                   
+                    #x.append(rl)
+                    #y.append(th)
+                    #graph(len(x) - 1, x, y)
+
+                    print("scale1: ", scale1, "scale2: ", scale2, "scale3: ", scale3, "scale4: ", scale4)
+                    #print("scale1: ", scale1, "scale2: ", scale2, "scale4:", scale4)
+
                     time.sleep(0.1)
                    
             # print(s1)
@@ -288,14 +335,18 @@ if __name__ == "__main__": # main function
             print("error: ", e)
 
         # create a video
-        video = anim_created.to_html5_video()
-        html = display.HTML(video)
-        display.display(html)
+        #video = anim_created.to_html5_video()
+        #html = display.HTML(video)
+        #display.display(html)
 
         weighing_system.s1.pause()
         weighing_system.s1.cancel()
         weighing_system.s2.pause()
         weighing_system.s2.cancel()
+        weighing_system.s3.pause()
+        weighing_system.s3.cancel()
+        weighing_system.s4.pause()
+        weighing_system.s4.cancel()
         pi.stop()
 
     # program begins to execute
